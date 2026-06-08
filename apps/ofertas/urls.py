0a32b6e99e9a06ed18c8c1304a55ca49c7c2cef4
Bruno <brunocamors2016@gmail.com>
2026-06-08 03:14:05 -0300

    feat(ofertas): publicacion de oferta funcionando

--- apps/ofertas/urls.py
+++ apps/ofertas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.lista_ofertas, name='lista_ofertas'),
-    # path('crear/', views.crear_oferta, name='crear_oferta'),
+    path('crear/', views.crear_oferta, name='crear_oferta'),
+    
 ]
--- apps/ofertas/views.py
+++ apps/ofertas/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import OfertaForm
+from .services import crear_oferta_laboral
 
-# Create your views here.
+@login_required
+def crear_oferta(request):
+    es_oferente = hasattr(request.user, 'oferente')
+    if not es_oferente:
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = OfertaForm(request.POST)
+        if form.is_valid():
+            crear_oferta_laboral(request.user, form)
+            return redirect('home')
+    else:
+        form = OfertaForm()
+
+    return render(request, 'Ofertas/publicar-empleo.html', {'form': form})
