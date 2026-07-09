3fb2bbd9dad1348cbd779d8c0df9b96bdf3194ef
Valentin Fluss <valentinfluss92@gmail.com>
2026-07-08 23:58:44 -0300

    refactor(moderacion): Service Layer + get_object_or_404 centralizado

--- apps/moderacion/urls.py
+++ apps/moderacion/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('cursos/crear/', views.crear_curso, name='mod_crear_curso'),
     path('cursos/<int:curso_id>/modificar/', views.modificar_curso, name='mod_modificar_curso'),
     path('cursos/<int:curso_id>/baja/', views.dar_de_baja_curso, name='mod_dar_de_baja_curso'),
-    
+
     path('categorias/', views.listar_categorias, name='mod_listar_categorias'),
     path('categorias/crear/', views.crear_categoria, name='mod_crear_categoria'),
     path('categorias/<int:categoria_id>/modificar/', views.modificar_categoria, name='mod_modificar_categoria'),
@@ -16,7 +16,7 @@ urlpatterns = [
     path('empresas/<int:pk>/', views.detalle_empresa, name='mod_detalle_empresa'),
     path('empresas/<int:pk>/aprobar/', views.aprobar_empresa, name='mod_aprobar_empresa'),
     path('empresas/<int:pk>/rechazar/', views.rechazar_empresa, name='mod_rechazar_empresa'),
-    
+
     path('ofertas/', views.listar_ofertas, name='mod_listar_ofertas'),
     path('oferta/<int:pk>/detalle/', views.detalle_oferta_json, name='mod_detalle_oferta'),
     path('oferta/<int:pk>/aprobar/', views.aprobar_oferta, name='mod_aprobar_oferta'),
